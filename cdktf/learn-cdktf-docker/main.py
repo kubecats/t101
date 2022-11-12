46340d3246566eeca18e2141f6273962a9831693
lhs <lhs.shcard@gmail.com>
2022-11-13 01:11:36 +0900

    221113

---
+++ cdktf/learn-cdktf-docker/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, TerraformStack
+
+# cdktf ver 0.13 이전
+from cdktf_cdktf_provider_docker import Image
+from cdktf_cdktf_provider_docker import Container
+from cdktf_cdktf_provider_docker import DockerProvider
+
+# cdktf ver = 0.13
+# from cdktf_cdktf_provider_docker.image import Image
+# from cdktf_cdktf_provider_docker.container import Container
+# from cdktf_cdktf_provider_docker.provider import DockerProvider
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        DockerProvider(self, 'docker')
+
+        docker_image = Image(self, 'nginxImage',
+            name='nginx:latest',
+            keep_locally=False)
+
+        Container(self, 'nginxContainer',
+            name='tutorial',
+            image=docker_image.name,
+            ports=[{
+                'internal': 80,
+                'external': 8000
+            }])
+
+
+app = App()
+MyStack(app, "learn-cdktf-docker")
+
+app.synth()
---
+++ cdktf/learn-cdktf/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, NamedRemoteWorkspace, TerraformStack, TerraformOutput, RemoteBackend
+from cdktf_cdktf_provider_aws import AwsProvider, ec2
+from imports.vpc import Vpc
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        AwsProvider(self, "AWS", region="ap-northeast-2")
+
+        instance = ec2.Instance(self, "compute",
+                                ami="ami-09cf633fe86e51bf0",
+                                instance_type="t2.micro",
+                                )
+
+        TerraformOutput(self, "public_ip",
+                        value=instance.public_ip,
+                        )
+
+
+app = App()
+stack = MyStack(app, "aws_instance")
+
+app.synth()
