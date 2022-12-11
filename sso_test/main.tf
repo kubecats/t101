6603f0d57b0812a28732b4a48f56b9714820a186
lhs <lhs.shcard@gmail.com>
2022-12-11 21:30:00 +0900

    sso

--- sso_test/main.tf
+++ sso_test/main.tf
@@ -123,34 +123,3 @@ variable "account_info" {
   description = "account_info"
   type        = map(any)
 }
-
-account_info = {
-  universal-ntw = {
-    account_no    = "11111111111",
-    account_label = "T101 test"
-  }
-}
-
-## SSO User ##
-## user_name, emails은 유일한 값
-user_info = {
-  no1 = {
-    display_name    = "t101(t101) - hs",
-    user_name = "hs",
-    given_name = "hs",
-    family_name= "lee",
-    emails = "hs.lee@amazon.com"
-  }
-}
-
-## SSO Group ##
-group_info = {
-  no1 = {
-    display_name    = "T101-Admin",
-    description = "T101 - Operation Admin"
-  },
-  no2 = {
-    display_name    = "T202-Admin",
-    description = "T202 - Admin"
-  }
-}
---
+++ sso_test/sso.tfvars
@@ -0,0 +1,30 @@
+account_info = {
+  universal-ntw = {
+    account_no    = "11111111111",
+    account_label = "T101 test"
+  }
+}
+
+## SSO User ##
+## user_name, emails은 유일한 값
+user_info = {
+  no1 = {
+    display_name    = "t101(t101) - hs",
+    user_name = "hs",
+    given_name = "hs",
+    family_name= "lee",
+    emails = "hs.lee@amazon.com"
+  }
+}
+
+## SSO Group ##
+group_info = {
+  no1 = {
+    display_name    = "T101-Admin",
+    description = "T101 - Operation Admin"
+  },
+  no2 = {
+    display_name    = "T202-Admin",
+    description = "T202 - Admin"
+  }
+}
